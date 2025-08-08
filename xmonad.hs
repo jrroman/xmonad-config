5c7966e81606a7c4cb27f19b06e361b77f26302a
John Roman <john@sprig.com>
2025-08-07 22:42:17 -0400

    inital commit; xmonad config from thinkpad

---
+++ xmonad.hs
@@ -0,0 +1,48 @@
+import XMonad
+
+import XMonad.Util.EZConfig (additionalKeys, additionalKeysP)
+import XMonad.Util.Ungrab
+import Graphics.X11.ExtraTypes.XF86
+-- Layout
+import XMonad.Layout.Spacing
+import XMonad.Layout.ThreeColumns
+-- Hooks
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.StatusBar
+import XMonad.Hooks.StatusBar.PP
+
+
+main :: IO ()
+main = xmonad . ewmhFullscreen . ewmh . xmobarProp $ myConfig
+
+myConfig = def
+    { terminal   = "alacritty"
+    , layoutHook = smartSpacing 2 $ myLayout
+    }
+  `additionalKeysP`
+    [ ("M-S-z", spawn "xscreensaver-command -lock")
+    , ("M-S-s", spawn "systemctl suspend")
+    , ("M-S-o", spawn "obsidian")
+    , ("M-d", spawn "dbeaver-ce")
+    , ("M-f", spawn "firefox")
+    , ("M-s", spawn "slack")
+    , ("M-m", spawn "spotify")
+    , ("M-z", spawn "zoom")
+    , ("M-<Print>", spawn "scrot -s")
+    ]
+  `additionalKeys`
+    [ ((0, xF86XK_AudioLowerVolume), spawn "amixer -q sset Master 2%-")
+    , ((0, xF86XK_AudioRaiseVolume), spawn "amixer -q sset Master 2%+")
+    , ((0, xF86XK_AudioMute), spawn "amixer set Master toggle")
+    , ((0, xF86XK_MonBrightnessUp), spawn "backlightctl -set 2%+")
+    , ((0, xF86XK_MonBrightnessDown), spawn "backlightctl -set 2%+")
+    ]
+
+myLayout = tiled ||| Mirror tiled ||| Full ||| threeCol
+  where
+    threeCol = ThreeColMid nmaster delta ratio
+    tiled    = Tall nmaster delta ratio
+    nmaster  = 1      -- Default number of windows in the master pane
+    ratio    = 1/2    -- Default proportion of screen occupied by master pane
+    delta    = 3/100  -- Percent of screen to increment by when resizing panes
