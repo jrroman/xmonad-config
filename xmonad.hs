fc3ff324be2b9719b7aa0f78220c0cb2108d303b
jr <jrrmod@gmail.com>
2025-10-02 07:38:00 -0400

    use lux for backlight control

--- xmonad.hs
+++ xmonad.hs
@@ -59,8 +59,8 @@ myConfig = def
     [ ((0, xF86XK_AudioLowerVolume), spawn "amixer -q sset Master 2%-")
     , ((0, xF86XK_AudioRaiseVolume), spawn "amixer -q sset Master 2%+")
     , ((0, xF86XK_AudioMute), spawn "amixer set Master toggle")
-    , ((0, xF86XK_MonBrightnessUp), spawn "backlightctl -set 2%+")
-    , ((0, xF86XK_MonBrightnessDown), spawn "backlightctl -set 2%+")
+    , ((0, xF86XK_MonBrightnessUp), spawn "lux -a 10%")
+    , ((0, xF86XK_MonBrightnessDown), spawn "lux -s 10%")
     ]
 
 myScratchpads :: [NamedScratchpad]
