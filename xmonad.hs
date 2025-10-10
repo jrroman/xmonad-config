473c16480ba3150b57583a791bffd1e303b0874c
jr <jrrmod@gmail.com>
2025-10-10 10:48:15 -0400

    invert dwindle direction for smaller screens

--- xmonad.hs
+++ xmonad.hs
@@ -176,7 +176,7 @@ myLayout = dwindle ||| tiled ||| Mirror tiled ||| Full ||| threeCol
   where
     threeCol = ThreeColMid nmaster delta ratio
     tiled    = ResizableTall nmaster delta ratio []
-    dwindle  = Dwindle L CW 1.5 1.1
+    dwindle  = Dwindle L CCW 1.5 1.1
     nmaster  = 1      -- Default number of windows in the master pane
     ratio    = 1/2    -- Default proportion of screen occupied by master pane
     delta    = 3/100  -- Percent of screen to increment by when resizing panes
