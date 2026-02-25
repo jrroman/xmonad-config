b07ef4acfe0891e9a2ef02df2ca05e625720994d
jr <jrrmod@gmail.com>
2026-02-25 14:20:36 -0500

    add new layout for dwindle spiral to the right

--- xmonad.hs
+++ xmonad.hs
@@ -180,11 +180,12 @@ myStartupHook = composeAll
   , spawnOn "1" "emacs --daemon"
   ]
 
-myLayout = dwindle ||| tiled ||| Mirror tiled ||| Full ||| threeCol
+myLayout = dwindle ||| dwindleR ||| tiled ||| Mirror tiled ||| Full ||| threeCol
   where
     threeCol = ThreeColMid nmaster delta ratio
     tiled    = ResizableTall nmaster delta ratio []
     dwindle  = Dwindle L CCW 1.5 1.1
+    dwindleR = Dwindle R CW 1.5 1.1
     nmaster  = 1      -- Default number of windows in the master pane
     ratio    = 1/2    -- Default proportion of screen occupied by master pane
     delta    = 3/100  -- Percent of screen to increment by when resizing panes
